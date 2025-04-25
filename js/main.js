f12ade4c09737b67c386a04b30e74a54a9262822
Patri-GL <patrigl1986@gmail.com>
2025-04-25 07:50:10 +0100

    Exercise from April 24

--- js/main.js
+++ js/main.js
@@ -42,4 +42,28 @@ btnadd.addEventListener('click', (event) => {
        extraform.classList.toggle("collapsed");
   });
 
+  const btnsearch = document.querySelector ('.js_button-search')
+  const searchdesc = document.querySelector ('.js_in_search_desc')
+
+  btnsearch.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    const descrSearchText = searchdesc.value;
+    const kittenDesc1 =
+  "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+  const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo."
+
+  const kittenDesc3 = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta."
+
+  if (kittenDesc1.includes(descrSearchText)) {
+    listElement.innerHTML = kittenOne
+  }
+  
+  if (kittenDesc2.includes(descrSearchText)) {
+    listElement.innerHTML += kittenTwo
+  }
+  
+  if (kittenDesc3.includes(descrSearchText)) {
+    listElement.innerHTML += kittenThree
+  }
+  });
 
