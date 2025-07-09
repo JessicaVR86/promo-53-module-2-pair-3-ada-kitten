29b25d495aeaaf3a46f9da031bd9f126eee13096
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-07-09 11:57:47 +0200

    clean comments

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ const kittenDataList = [
     image: "https://dev.adalab.es/maine-coon-cat.webp",
     name: "Cielo",
     desc: "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.",
-    // race: 'Maine Coon',
+
   },
 ];
 
@@ -56,24 +56,6 @@ function renderrace(kittenrace) {
   return breedText;
 }
 
-// function renderKitten(kittenData) {
-//   const breedText = renderrace(kittenData.race);
-
-//  listElement.innerHTML += `
-//    <li class="card">
-//      <article>
-//          <img class="card_img" src="${kittenData.image}" />
-//          <h3 class="card_title">${kittenData.name.toUpperCase()}</h3>
-//          <h4 class="card_race">${breedText}</h4>
-//          <p class="card_description">${kittenData.desc}</p>
-//      </article>
-//    </li>`;
-//}
-
-
-//renderKittenList(kittenDataList);
-
-//  ejercicio bucles
 
 function renderKittenList(kittenData) {
   const breedText = renderrace(kittenData.race);
@@ -89,19 +71,14 @@ function renderKittenList(kittenData) {
 return kittenItems;
 };
 
-listElement.innerHTML += kittenItems;
 
 
-  renderKittenList(kittenDataList[0]);
-  renderKittenList(kittenDataList[1]);
-  renderKittenList(kittenDataList[2]);
+
+ listElement.innerHTML +=  renderKittenList(kittenDataList[0]);
+ listElement.innerHTML +=  renderKittenList(kittenDataList[1]);
+ listElement.innerHTML +=  renderKittenList(kittenDataList[2]);
 
 
-  //Completa el código:
-  //Vaciamos el innerHTML del <ul> de la página.
-  //Iteramos sobre el listado de gatitos
-  //Y por cada iteración pintamos un gatito.
-  //utilizando la función renderKitten(kittenItem)
 
 
 btnadd.addEventListener("click", handleClickNewCatForm);
@@ -125,6 +102,7 @@ btnsearch.addEventListener("click", filterKitten);
 
 const buttonCancelForm = document.querySelector(".js-btn-cancel");
 const cancelNewKitten = (event) => {
-  addForm.innerHTML = addForm.innerHTML;
+  event.preventDefault();
+  addForm.reset();
 };
 buttonCancelForm.addEventListener("click", cancelNewKitten);
